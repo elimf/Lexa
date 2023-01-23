9ad845b69d08cc0bf962952107617eae0938b585
Elim Florvil <elim.florvil@icloud.com>
2023-01-23 13:43:09 +0100

    feat/clear command

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
 const { DISCORD_TOKEN } = process.env;
-
+const ytdl = require("ytdl-core");
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -20,7 +20,7 @@ const newMembre = (member) => {
     .catch(console.error);
   // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
 };
-const onMessage = (message) => {
+const onMessage =  async (message) => {
   if (message.author.bot) return;
   if (message.content === "!hello") {
     message.channel.send("Welcome to the server, " + message.author + "!");
@@ -35,7 +35,55 @@ const onMessage = (message) => {
   } else {
     message.channel.send("You need to join a voice channel first!");
   }
+
+  if (message.content === "zebi") {
+    message.channel.send("Zebi Zebi");
+  }
+// * CLEAR
+  if (message.content === "!clear") {
+    message.channel.messages.fetch({ limit: 10 }).then((messages) => {
+      message.channel.bulkDelete(messages);
+    });
+  }
+  // * YOUTUBE
+  if (message.content === "!play") {
+    const streamOptions = { seek: 0, volume: 1 };
+    const voiceChannel = message.member.voice.channel;
+    if (!voiceChannel)
+      return message.reply("Please join a voice channel first!");
+    voiceChannel
+      .join()
+      .then((connection) => {
+        const stream = ytdl(
+          "https://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID",
+          { filter: "audioonly" }
+        );
+        const dispatcher = connection.play(stream, streamOptions);
+        dispatcher.on("end", () => voiceChannel.leave());
+      })
+      .catch(console.error);
+  }
+  // *BAN
+  if (message.content.startsWith("!ban")) {
+    let member = message.mentions.members.first();
+    if (!member)
+      return message.reply("Please mention a valid member of this server.");
+    if (!member.bannable) return message.reply("I cannot ban this user.");
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No reason provided.";
+    member
+      .ban(reason)
+      .catch((error) =>
+        message.reply(
+          `Sorry ${message.author} I couldn't ban because of : ${error}`
+        )
+      );
+    message.reply(
+      `${member.user.tag} has been banned by ${message.author.tag} because of: ${reason}`
+    );
+  }
 };
+
 const onReady = () => {
   console.log(`${client.user.tag} à votre service`);
 };
