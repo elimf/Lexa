208735c77771cf59f97066c487dadb14995ec518
Elim Florvil <elim.florvil@icloud.com>
2023-01-24 02:13:57 +0100

    config.json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//Get DISCORD_TOKEN
 require("dotenv").config();
 const { Client, GatewayIntentBits } = require("discord.js");
-const { DISCORD_TOKEN } = process.env;
+// const { DISCORD_TOKEN } = process.env;
+const config = require("./config.json");
 const ytdl = require("ytdl-core");
 const client = new Client({
   intents: [
@@ -10,6 +10,7 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
+const prefix = "!";
 
 const newMembre = (member) => {
   member
@@ -20,49 +21,57 @@ const newMembre = (member) => {
     .catch(console.error);
   // On pourrait catch l'erreur autrement ici (l'utilisateur a peut être désactivé les MP)
 };
-const onMessage =  async (message) => {
+const onMessage = async (message) => {
   if (message.author.bot) return;
-  if (message.content === "!hello") {
-    message.channel.send("Welcome to the server, " + message.author + "!");
-  }
-  if (message.member.voice.channel) {
-    message.member.voice.channel
-      .join()
-      .then((connection) => {
-        message.channel.send("I have joined the voice channel!");
-      })
-      .catch(console.log);
-  } else {
-    message.channel.send("You need to join a voice channel first!");
-  }
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
 
-  if (message.content === "zebi") {
-    message.channel.send("Zebi Zebi");
-  }
-// * CLEAR
-  if (message.content === "!clear") {
-    message.channel.messages.fetch({ limit: 10 }).then((messages) => {
-      message.channel.bulkDelete(messages);
-    });
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(" ");
+  const command = args.shift().toLowerCase();
+  // if (message.member.voice.channel) {
+  //   message.member.voice.channel
+  //     .join()
+  //     .then((connection) => {
+  //       message.channel.send("I have joined the voice channel!");
+  //     })
+  //     .catch(console.log);
+  // } else {
+  //   message.channel.send("You need to join a voice channel first!");
+  // }
+
+  // * CLEAR
+  if (command === "clear") {
+     const numArgs = args.map((x) => parseFloat(x));
+     if (numArgs.length === 0) {
+       message.channel.messages.fetch({ limit: 1 }).then((messages) => {
+         message.channel.bulkDelete(messages);
+       });
+     } else {
+       message.channel.messages.fetch({ limit: numArgs }).then((messages) => {
+         message.channel.bulkDelete(messages);
+       });
+     }
+  
   }
   // * YOUTUBE
-  if (message.content === "!play") {
-    const streamOptions = { seek: 0, volume: 1 };
-    const voiceChannel = message.member.voice.channel;
-    if (!voiceChannel)
-      return message.reply("Please join a voice channel first!");
-    voiceChannel
-      .join()
-      .then((connection) => {
-        const stream = ytdl(
-          "https://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID",
-          { filter: "audioonly" }
-        );
-        const dispatcher = connection.play(stream, streamOptions);
-        dispatcher.on("end", () => voiceChannel.leave());
-      })
-      .catch(console.error);
-  }
+  // if (message.content === "!play") {
+  //   const streamOptions = { seek: 0, volume: 1 };
+  //   const voiceChannel = message.member.voice.channel;
+  //   if (!voiceChannel)
+  //     return message.reply("Please join a voice channel first!");
+  //   voiceChannel
+  //     .join()
+  //     .then((connection) => {
+  //       const stream = ytdl(
+  //         "https://www.youtube.com/watch?v=YOUTUBE_VIDEO_ID",
+  //         { filter: "audioonly" }
+  //       );
+  //       const dispatcher = connection.play(stream, streamOptions);
+  //       dispatcher.on("end", () => voiceChannel.leave());
+  //     })
+  //     .catch(console.error);
+  // }
   // *BAN
   if (message.content.startsWith("!ban")) {
     let member = message.mentions.members.first();
@@ -91,5 +100,6 @@ const onReady = () => {
 client.on("ready", onReady);
 client.on("messageCreate", onMessage);
 client.on("guildMemberAdd", newMembre);
+client.on("error", console.error);
 
-client.login(DISCORD_TOKEN);
+client.login(config.BOT_TOKEN);
